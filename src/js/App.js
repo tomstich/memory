9d519277410178ea3d6795c3cf23b90263761bec
Tom Stich <tomstich@Toms-MacBook-Pro.local>
2019-08-19 15:11:35 +0200

    refactor(app): improve select method

--- src/js/App.js
+++ src/js/App.js
@@ -9,52 +9,81 @@ var state = {
     firstSelectedCardId: undefined,
     secondSelectedCardId: undefined,
     firstSelectedCard: undefined,
-    secondSelectedCard: undefined
+    secondSelectedCard: undefined,
+    nextClickReset: false,
 }
 
 function checkForPair() {
     // check if firstSelectedCard and secondSelectedCard have the same value/picture,
-    if (state.firstSelectedCard === state.secondSelectedCard) {
+    if (state.firstSelectedCard === state.secondSelectedCard && state.firstSelectedCardId !== undefined) {
         console.log(`You found a pair: ${state.firstSelectedCard} : ${state.secondSelectedCard}`);
+    } else {
+        console.log('No pair found')
     }
 
-    // reset the state after checking for a pair
-    state = {
+    if (state.nextClickReset) {
+        toggleCard(state.firstSelectedCardId);
+        toggleCard(state.secondSelectedCardId);
+
+        state = {
         ...state,
-        firstSelectedCardId: undefined,
-        firstSelectedCard: undefined,
-        secondSelectedCardId: undefined,
-        secondSelectedCard: undefined,
+            firstSelectedCardId: undefined,
+            firstSelectedCard: undefined,
+            secondSelectedCardId: undefined,
+            secondSelectedCard: undefined,
+            nextClickReset: false,
+        }
     }
+
 }
 
-// this part is needed to check if a pair got selected
-document.addEventListener('click', function (event) {
-    if (!event.target.matches('.card')) return;
-    event.preventDefault();
+function toggleCard(cardId) {
+    var card = document.getElementById(cardId);
 
-    // console.log(event.target.innerHTML);
-    if (!state.firstSelectedCardId) {
+    card.classList.toggle("show");
+    card.classList.toggle("disabled");
+}
+
+var select = (event) => {
+    if (!state.firstSelectedCardId && event.target.id) {        
         state = {
             ...state,
+            nextClickReset: false,
             firstSelectedCardId: event.target.id,
             firstSelectedCard: event.target.innerHTML
         }
-    } else if (state.firstSelectedCardId != event.target.id) {
+        
+        toggleCard(event.target.id);
+    } else if (state.firstSelectedCardId && !state.secondSelectedCardId) {
         state = {
             ...state,
             secondSelectedCardId: event.target.id,
             secondSelectedCard: event.target.innerHTML
         }
-    }
+        
+        if (state.firstSelectedCardId !== state.secondSelectedCardId) {
+            toggleCard(event.target.id);
+            checkForPair();
 
-    // Check if a second card got clicked, if yes, check for pair
-    if (state.secondSelectedCardId) {
-        checkForPair()
+            state =  {
+                ...state,
+                nextClickReset: true,
+            }
+        }
+    } else {
+        // after updating the state, check for pair
+        checkForPair();
     }
-}, false);
+}
 
 game.renderMemoryBoard();
 
+// we call the function select to update the state after each click
+document.addEventListener('click', function (event) {
+    if (!event.target.matches('.card')) return;
+    event.preventDefault();
+
+    select(event);
+}, false);
 
 
