673f4f5237a8b6f485e9f151dae12d6ce1f712ad
tomstich <tom.stich@jimdo.comm>
2019-08-22 15:48:22 +0200

    fix(MemoryBoard): fix typo

--- src/js/MemoryBoard.js
+++ src/js/MemoryBoard.js
@@ -39,7 +39,7 @@ export class MemoryBoard {
     }
 
     shuffleArray(array) {
-        for (let i = a.length - 1; i > 0; i--) {
+        for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
